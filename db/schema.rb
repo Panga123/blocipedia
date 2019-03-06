83d1c70feb2e2788dbf4ec35e7c6e523ae645766
Ann Murray Dunning <ann@amurraypr.com>
2019-03-05 22:56:46 -0800

    Checkpoint User Story Crud Not finished

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,10 @@ ActiveRecord::Schema.define(version: 2019_02_15_052323) do
   create_table "wikis", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.boolean "private"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wikis_on_user_id"
   end
 
 end
